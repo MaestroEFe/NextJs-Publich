b1d4f28baa0aa1f9af96e6f9dbee05a2e8fb8d28
MaestroEfem <victor.akeks@gmail.com>
2025-07-03 06:25:14 +0100

    branch for contact form handling

--- AuthenticationSystem/apps/web/.next/server/webpack-runtime.js
+++ AuthenticationSystem/apps/web/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e77f73130589a3a7")
+/******/ 		__webpack_require__.h = () => ("7f132b4722597138")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
